2d861f76a4f6cffd2b6b78d4a4d84628c5898ac9
Shawn <shawn.rushefsky@gmail.com>
2024-10-08 12:52:12 -0400

    also yml

--- .github/scripts/validate-all-workflows.sh
+++ .github/scripts/validate-all-workflows.sh
@@ -3,7 +3,7 @@
 api_spec_dir="api-specs"
 
 # Spec can be in json or yaml
-for spec in $(find $api_spec_dir -type f -name "*.json" -o -name "*.yaml"); do
+for spec in $(find $api_spec_dir -type f -name "*.json" -o -name "*.yaml" -o -name "*.yml"); do
   echo "Validating $spec"
   npx --yes @apidevtools/swagger-cli validate $spec
 done
