2a192d8ad162a97b92fc6cc3e28fb39f273f74d2
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2024-10-08 12:55:14 -0400

    validate all schemas in the specs folder (#84)

---
+++ .github/scripts/validate-all-workflows.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+set -e
+
+api_spec_dir="api-specs"
+
+# Spec can be in json or yaml
+for spec in $(find $api_spec_dir -type f -name "*.json" -o -name "*.yaml" -o -name "*.yml"); do
+  echo "Validating $spec"
+  npx --yes @apidevtools/swagger-cli validate $spec
+done
