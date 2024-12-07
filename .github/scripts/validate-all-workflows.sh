4f4b28701d77c6a870a08b76b8f12ddbccecc32c
Kyle Dodson <kyledodson@gmail.com>
2024-12-07 15:42:17 -0800

    Update GitHub Actions

--- .github/scripts/validate-all-workflows.sh
+++ .github/scripts/validate-all-workflows.sh
@@ -1,10 +1,10 @@
-#! /usr/bin/bash
+#!/bin/bash
 set -e
 
 api_spec_dir="api-specs"
 
-# Spec can be in json or yaml
+# OpenAPI Specification documents may be JSON or YAML
 for spec in $(find $api_spec_dir -type f -name "*.json" -o -name "*.yaml" -o -name "*.yml"); do
   echo "Validating $spec"
-  npx --yes @apidevtools/swagger-cli validate $spec
+  npx --yes mintlify openapi-check $spec
 done
