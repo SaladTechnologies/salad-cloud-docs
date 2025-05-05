15e3bde37bae213f5064d854cfebd120360d9628
Kyle Dodson <kyledodson@gmail.com>
2025-05-05 11:11:01 -0700

    Convert to JavaScript

--- vimeo.js
+++ vimeo.js
@@ -1,1 +1,7 @@
-<script type="text/javascript" defer="defer" src="https://extend.vimeocdn.com/ga4/109034429.js"></script>
+const script = document.createElement('script')
+script.type = 'text/javascript'
+script.id = 'vimeo-script-loader'
+script.async = true
+script.defer = true
+script.src = 'https://extend.vimeocdn.com/ga4/109034429.js'
+document.head.appendChild(script)
