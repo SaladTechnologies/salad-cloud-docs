19f925770f6cf2ee3502445b7714e607b1ac1597
Kyle Dodson <kyledodson@gmail.com>
2025-05-05 11:23:01 -0700

    Add Vimeo analytics (#205)

---
+++ vimeo.js
@@ -0,0 +1,7 @@
+const script = document.createElement('script')
+script.type = 'text/javascript'
+script.id = 'vimeo-script-loader'
+script.async = true
+script.defer = true
+script.src = 'https://extend.vimeocdn.com/ga4/109034429.js'
+document.head.appendChild(script)
